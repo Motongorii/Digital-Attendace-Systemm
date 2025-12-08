009c1e7e5b1e516dafca09993413ccb0e949704f
Anthony Motongori <antomotongori@gmail.com>
2025-12-08 01:15:06 -0800

    Add release.sh: run migrations, collectstatic, then start Gunicorn

--- release.sh
+++ release.sh
@@ -1,8 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-echo "[Release] Running Django migrations..."
-python manage.py migrate --noinput
-echo "[Release] Migrations complete. Starting Gunicorn..."
+echo "Waiting for database (brief pause)"
+sleep 2
 
-exec gunicorn attendance_system.wsgi:application --bind 0.0.0.0:8080 --workers 3
+echo "Running Django migrations..."
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Starting Gunicorn..."
+exec gunicorn attendance_system.wsgi:application --bind 0.0.0.0:8080 --workers 2
